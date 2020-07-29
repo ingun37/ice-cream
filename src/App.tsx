6906b293eaccabccb87bbb1bf836caed3903d1d6
Ingun <ingun37@gmail.com>
2020-07-29 23:25:07 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as io from "io-ts";
+import icecreamJson from "./icecreams.json";
+import { bimap, fold } from 'fp-ts/lib/Either';
+import { pipe } from 'fp-ts/lib/function';
+
+const IcecreamValidator = io.type({
+  name: io.string,
+  price: io.number,
+});
+
+const IceCreamArrayValidator = io.array(IcecreamValidator);
+
+type IIcecream = io.TypeOf<typeof IcecreamValidator>;
+type IIcecreamArray = io.TypeOf<typeof IceCreamArrayValidator>;
+
+const icecreamDatas = IceCreamArrayValidator.decode(icecreamJson)
+
+if (icecreamDatas._tag == "Right") {
+  console.log(icecreamDatas.right)
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          pipe(icecreamDatas, fold(
+            l => (<div>error</div>),
+            r => {
+              return (
+                <div>
+                  {r.map(x => (<div>{x.name}</div>))}
+                </div>
+              )
+            }))
+        }
       </header>
     </div>
   );
