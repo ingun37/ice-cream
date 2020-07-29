9656f4c923bcaec1522c28369e0dcd7c0acff8e8
Ingun <ingun37@gmail.com>
2020-07-30 01:01:31 +0900

    aoeu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as io from "io-ts";
 import icecreamJson from "./icecreams.json";
-import { bimap, fold } from 'fp-ts/lib/Either';
+import { bimap, fold, Either, left, right } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/function';
+import * as _ from 'lodash';
+import { makeStyles } from '@material-ui/core/styles';
+import { GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { from } from 'rxjs';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+    width: '100%'
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  image: {
+    height: '100%'
+  },
+  skeleton: {
+    height: '100%'
+  }
+}));
 
 const IcecreamValidator = io.type({
   name: io.string,
@@ -23,23 +57,79 @@ if (icecreamDatas._tag == "Right") {
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {
-          pipe(icecreamDatas, fold(
-            l => (<div>error</div>),
-            r => {
+  return pipe(icecreamDatas, fold(
+    l => (<div>error</div>),
+    r => {
+      const sortedPrices = _.uniq(r.map(x => x.price)).sort((a, b) => a - b)
+      return (
+        <div>
+          {
+            sortedPrices.map(price => {
+              const icecreams = r.filter(x => x.price == price).map(x => x.name);
               return (
                 <div>
-                  {r.map(x => (<div>{x.name}</div>))}
+                  {price}
+                  {SingleLineGridList(icecreams)}
                 </div>
               )
-            }))
-        }
-      </header>
+            })
+          }
+        </div>
+      )
+    })
+
+  );
+}
+
+function SingleLineGridList(icecreams: string[]) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={4}>
+        {icecreams.map((icecream) => (
+          <GridListTile key={icecream} cols={1}>
+            <IcecreamImage name={icecream}></IcecreamImage>
+            {/* <Skeleton variant="rect" className={classes.image}></Skeleton> */}
+            {/* <img src={Merona} alt={icecream} className={classes.image} /> */}
+            <GridListTileBar
+              title={icecream}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={null}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
 
+interface IcecreamImageProps {
+  name: string
+}
+function IcecreamImage({ name }: IcecreamImageProps) {
+  const classes = useStyles();
+
+  const [src, setSrc] = useState<Either<null, string | null>>(left(null))//((<Skeleton variant="rect" className={classes.skeleton}></Skeleton>))
+  useEffect(() => {
+    const subscription = from(import('./images/' + name + '.jpg')).subscribe({
+      next(x) {
+        setSrc(right(x.default))
+      },
+      error(e) {
+        setSrc(right(null))
+      }
+    });
+    return ()=>{
+      subscription.unsubscribe();
+    }
+  }, [])
+  return pipe(src, fold(
+    l => ((<Skeleton variant="rect" className={classes.skeleton}></Skeleton>)),
+    r => r ? (<img src={r} alt={name} className={classes.image}/>) : (<div>No Image</div>)
+  ))
+}
 export default App;
